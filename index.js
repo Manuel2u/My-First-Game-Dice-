ec57ee144476907c547723af37e762f04f2174cb
Emmanuel Dodoo <edodoo011@st.ug.edu.gh>
2022-06-15 16:55:29 +0000

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,50 @@ var player1 = prompt("Enter Player 1 name: ");
 
 var player2 = prompt("Enter Player 2 name: ");
 
-player1 = player1.toUpperCase();
+var firstLetter1 = player1.slice(0, 1);
 
-player2 = player2.toUpperCase();
+var player1FirstLetterToUpper = firstLetter1.toUpperCase();
+
+var restOfName1 = player1.slice(1, player1.length);
+
+restOfName1 = restOfName1.toLowerCase();
+
+var firstLetter2 = player2.slice(0, 1);
+
+var player2FirstLetterToUpper = firstLetter2.toUpperCase();
+
+restOfName2 = player2.slice(1, player2.length);
+
+restOfName2 = restOfName2.toLowerCase();
+
+player1 = player1FirstLetterToUpper + restOfName1;
+
+player2 = player2FirstLetterToUpper + restOfName2;
+
+if (player1.length > 5) {
+  alert("Player name should be only 5 characters maximum.")
+
+  function FatalError() {
+    Error.apply(this, arguments);
+    this.name = "FatalError";
+  }
+  FatalError.prototype = Object.create(Error.prototype);
+
+  throw new FatalError("Something went wrong!");
+}
+
+
+if (player2.length > 5) {
+  alert("Player name should be only 5 characters maximum.")
+
+  function FatalError() {
+    Error.apply(this, arguments);
+    this.name = "FatalError";
+  }
+  FatalError.prototype = Object.create(Error.prototype);
+
+  throw new FatalError("Something went wrong!");
+}
 
 document.querySelectorAll("p")[0].innerHTML = player1;
 
@@ -23,14 +64,18 @@ document.querySelectorAll("img")[0].setAttribute("src", "images/dice" + randomNu
 document.querySelectorAll("img")[1].setAttribute("src", "images/dice" + randomNumber2 + ".png");
 
 
-function rollADice () {
+function rollADice() {
 
   if (randomNumber1 > randomNumber2) {
-    document.querySelector("h1").innerHTML = "🚩"+player1+" WINS!";
+    document.querySelector("h1").innerHTML = "🚩" + player1 + " wins!";
   } else if (randomNumber1 === randomNumber2) {
-    document.querySelector("h1").innerHTML = "DRAW!";
+    document.querySelector("h1").innerHTML = "Draw!";
   } else {
-    document.querySelector("h1").innerHTML = player2+" WINS! 🚩";
+    document.querySelector("h1").innerHTML = player2 + " wins! 🚩";
   }
 
+}
+
+function playAgain() {
+  window.location.reload('Refresh')
 }
